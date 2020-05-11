9b911e9d764aaa4fc3eb0af6613951ead721302f
Ze Canelha <zecanelha@gmail.com>
2020-05-11 23:13:43 +0100

    Interactive Linechart using keyframes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import "./index.css";
 
 // import BarChart from "./components/BarCharts";
 // import InteractiveKeyframes from "./components/InteractiveKeyframes";
-import KeyframeLineChart from "./components/KeyframeLineChart";
+// import KeyframeLineChart from "./components/KeyframeLineChart";
+import InteractiveLineChart from "./components/InteractiveLineChart";
 
 const width = 600;
 const height = 450;
@@ -17,7 +18,11 @@ class App extends React.Component {
         <h1>Data Visualization with D3.js and React</h1>
         {/* <BarChart width={width} height={height}></BarChart> */}
         {/* <InteractiveKeyframes width={width} height={height}></InteractiveKeyframes> */}
-        <KeyframeLineChart width={width} height={height}></KeyframeLineChart>
+        {/* <KeyframeLineChart width={width} height={height}></KeyframeLineChart> */}
+        <InteractiveLineChart
+          width={width}
+          height={height}
+        ></InteractiveLineChart>
       </>
     );
   }
