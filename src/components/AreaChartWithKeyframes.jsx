ead6733fd6ee2d84c2fab1da1c3bbf10d930db89
Ze Canelha <zecanelha@gmail.com>
2020-05-14 21:28:13 +0100

    Datapoints ordered in a quickfix style - refactor next

--- src/components/AreaChartWithKeyframes.jsx
+++ src/components/AreaChartWithKeyframes.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import * as d3 from "d3";
-
-import DataPoints from "../datasets/pattern1.json";
+import update from "immutability-helper";
+import DataPoints from "../datasets/pattern2.json";
+import { curveMonotoneX } from "d3";
 
 const margin = { top: 20, rigth: 30, bottom: 30, left: 40 };
+
+let quickfix = null;
 
 export default class AreaChartWithKeyframes extends React.Component {
   constructor(props) {
@@ -12,6 +15,7 @@ export default class AreaChartWithKeyframes extends React.Component {
       datapoints: null,
       area: null,
     };
+    quickfix = this;
   }
 
   // Scales and axis
@@ -25,12 +29,109 @@ export default class AreaChartWithKeyframes extends React.Component {
   xAxis = d3.axisBottom().scale(this.xScale);
   yAxis = d3.axisLeft().scale(this.yScale);
 
-  areaGenerator = d3.area();
+  areaGenerator = d3.area().curve(curveMonotoneX);
+
+  drawKeyframe(coords) {
+    let svg = d3.select(this.refs.area);
+    let newPoints = {
+      time: Math.round(this.xScale.invert(coords[0])),
+      intensity: Math.round(this.yScale.invert(coords[1])),
+    };
+
+    // update datapoints in time
+
+    let datapoints = this.state.datapoints;
+    let needToUpdate = 0;
+    let index = 0;
+
+    for (index; index < datapoints.length; index++) {
+      if (datapoints[index].time < newPoints.time) continue;
+      else {
+        needToUpdate = index;
+        break;
+      }
+    }
+
+    if (needToUpdate !== 0) {
+      datapoints.splice(index, 0, newPoints);
+      this.setState({
+        datapoints: update(this.state.datapoints, {
+          datapoints: { $set: datapoints },
+        }),
+      });
+    } else {
+      this.setState({ datapoints: [...this.state.datapoints, newPoints] });
+    }
+
+    console.log(this.state.datapoints);
+
+    svg
+      .selectAll("circle")
+      .data(this.state.datapoints)
+      .enter()
+      .append("circle")
+      .attr("cx", this.xScale(newPoints.time))
+      .attr("cy", this.yScale(newPoints.intensity))
+      .attr("r", 4)
+      .attr("fill", "orange")
+      .on("mouseover", this.handleMouseOver)
+      .on("mouseout", this.handleMouseOut)
+      .call(
+        d3
+          .drag()
+          .on("start", this.draggStarted)
+          .on("drag", this.dragged)
+          .on("end", this.dragended)
+      );
+  }
+
+  handleMouseOver(d, i) {
+    d3.select(this)
+      .attr("fill", "orange")
+      .attr("r", "8")
+      .append("title")
+      .text(d.time + ", " + d.intensity);
+  }
+
+  handleMouseOut(d, i) {
+    d3.select(this).attr("fill", "orange").attr("r", "4");
+  }
+
+  draggStarted(d) {
+    console.log("drag started");
+  }
+  dragged(d, i) {
+    d3.select(this).attr("cx", d3.event.x).attr("cy", d3.event.y);
+
+    let x = Math.round(quickfix.xScale.invert(d3.event.x));
+    let y = Math.round(quickfix.yScale.invert(d3.event.y));
+
+    quickfix.quickfix2(x, y, i);
+  }
+  dragended(d) {
+    d3.select(this).text(d3.event.x + ", " + d3.event.y);
+  }
+
+  quickfix2(x, y, i) {
+    let newState = update(this.state, {
+      datapoints: {
+        [i]: {
+          time: { $set: x },
+          intensity: { $set: y },
+        },
+      },
+    });
+    this.setState(newState);
+  }
 
   componentDidMount() {
+    let theobject = this;
     const data = DataPoints.keyframes;
+
+    this.setState({ datapoints: data });
+
     // Set xScale domain according to the time defined in the config file
-    this.xScale.domain([0, d3.max(data, (d) => d.time + 10)]);
+    this.xScale.domain([0, 350]);
 
     // Y Scale is a fixed scale from 0 to 100%
     this.yScale.domain([100, 0]);
@@ -59,9 +160,19 @@ export default class AreaChartWithKeyframes extends React.Component {
       .attr("cy", (d) => this.yScale(d.intensity))
       .attr("r", 4);
 
-    const area = this.areaGenerator(data);
+    d3.select(this.refs.area).on("dblclick", function () {
+      let coords = d3.mouse(this);
+      theobject.drawKeyframe(coords);
+    });
+  }
 
-    this.setState({ area });
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.datapoints !== this.state.datapoints) {
+      console.log("A change in state occured");
+      const area = this.areaGenerator(this.state.datapoints);
+
+      this.setState({ area: area });
+    }
   }
 
   render() {
