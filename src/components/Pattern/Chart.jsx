0541fc6a0188b6a0f2df2a5c0bfe1b5fdb082c2e
Ze Canelha <zecanelha@gmail.com>
2020-09-17 15:16:50 +0100

    Resizing svg

--- src/components/Pattern/Chart.jsx
+++ src/components/Pattern/Chart.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Datapoints from "../../datasets/pattern2.json";
 import Pattern from "./Pattern";
-import update from "immutability-helper";
-import * as d3 from "d3";
 
 /*
     Recebe os datapoins e cria um componente que irá renderizar o SVG. Pode-se 
@@ -15,18 +13,50 @@ const margin = { top: 20, rigth: 30, bottom: 30, left: 40 };
 class Chart extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { data: Datapoints.keyframes };
+    this.state = { data: Datapoints.keyframes, width: null, height: null };
+    this.fitToContainer = this.fitToContainer.bind(this);
+  }
+
+  componentDidMount() {
+    this.fitToContainer();
+    window.addEventListener("resize", this.fitToContainer);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.fitToContainer);
+  }
+
+  fitToContainer() {
+    console.log("Resizing");
+
+    const { width } = this.state;
+    const { height } = this.state;
+
+    const currentWidth = this.refs.patternContainer.getBoundingClientRect()
+      .width;
+    const currentHeight = this.refs.patternContainer.getBoundingClientRect()
+      .height;
+
+    const shouldResize = width !== currentWidth || height !== currentHeight;
+
+    if (shouldResize) {
+      this.setState({ width: currentWidth, height: currentHeight });
+    }
   }
 
   render() {
+    const shouldRender = this.state.width !== null;
+
     return (
-      <div className="container">
-        <Pattern
-          {...this.state}
-          {...this.props}
-          {...margin}
-          addKeyFrame={this.addKeyFrame}
-        ></Pattern>
+      <div className="container" ref={"patternContainer"}>
+        {shouldRender && (
+          <Pattern
+            {...this.state}
+            {...this.props}
+            {...margin}
+            addKeyFrame={this.addKeyFrame}
+          ></Pattern>
+        )}
       </div>
     );
   }
